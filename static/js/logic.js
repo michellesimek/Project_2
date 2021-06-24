3b50d489c28a48c2b8fe6322d13d5948f41a1239
Michelle Simek <michellesimek@michelles-mbp.attlocal.net>
2021-06-23 21:43:35 -0500

    updated logic.js to load charts based off dropdown selection

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,17 +1,6 @@
-// check to ensure data is uploading correctly
-// d3.csv("/static/data/potholes_clean_noid.csv").then(function(data) {
-//     // loop through each data row and print out creation date
-//     // for (var i = 0; i < data.length; i++) {
-//     //     console.log(data[i].CREATION_DATE);
-//     // };
-//     data.forEach(function(data) {
-//         console.log(data.STATUS)
-//     });
-//     // print out all data from csv
-//     // console.log(data)
-// });
+var myMap;
+var markerGroup;
 
-// ---------------------------------------------------------------
 // svg container
 var svgHeight = 400;
 var svgWidth = 1000;
@@ -40,6 +29,37 @@ var chartGroup = svg.append("g")
 // Step 1: Plotly
 // initialize the page with default data
 function init() {
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+});
+
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+};
+
+// Create a new map
+myMap = L.map("map", {
+  center: [41.8781, -87.6298],
+  zoom: 10,
+  layers: [streetmap]
+});
+
+// Create a layer control containing our baseMaps
+// Be sure to add an overlay Layer containing the earthquake GeoJSON
+L.control.layers(baseMaps).addTo(myMap);
   // read in samples.json
   d3.csv("/static/data/potholes_clean_mnthfinal.csv").then((data) => {
 
@@ -85,96 +105,39 @@ function updateData() {
     // filter data based on year selected in dropdown menu
     let filteredData = data.filter(elt => elt.CREATION_YEAR == dataSelection);
     console.log(filteredData);
+    console.log(dataSelection);
 
-    // create function to group data by
-    function groupBy(objectArray, property) {
-        return objectArray.reduce(function (acc, obj) {
-          let key = obj[property]
-          if (!acc[key]) {
-            acc[key] = []
-          }
-          acc[key].push(obj)
-          return acc
-        }, {})
-    };
-
-    function groupBySum(objectArray, property) {
-      return objectArray.reduce(function (acc, obj) {
-        let key = obj[property]
-        acc[key] = (acc[key] || 0) + 1
-        return acc
-      }, {})
-    };
-
-    // call groupBy function to group filteredData by MOST_RECENT_ACTION
-    let groupedAction = groupBy(filteredData, 'STATUS');
-    console.log(groupedAction);
-
-    buildGeoMap(filteredData);
     buildBarChart(dataSelection, filteredData);
     buildLineChart(dataSelection, filteredData);
+    buildGeoMap(filteredData, dataSelection);
 
   })
 };
 
-function buildGeoMap() {
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-});
+function buildGeoMap(filteredData, dataSelection) {
 
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-});
-
-var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-};
-
-// var overlayMaps = {
-//     Earthquakes: earthquakes
-// };
-// Read markers data from data.csv
-d3.csv("/static/data/potholes_clean_mnthfinal.csv", function(csvString) {
-
-  // Use PapaParse to convert string to array of objects
-  var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
-
-  // For each row in data, create a marker and add it to the map
-  // For each row, columns `Latitude`, `Longitude`, and `Title` are required
-  for (var i in data) {
-    var row = data[i];
-
-    var marker = L.marker([row.LATITUDE, row.LONGITUDE], {
-      opacity: 1
-    }).bindPopup(row.ZIP);
-    
-    marker.addTo(map);
-  }
-
-});
-
-// Create a new map
-var myMap = L.map("map", {
-    center: [41.8781, -87.6298],
-    zoom: 2,
-    layers: [streetmap]
-});
-
-// Create a layer control containing our baseMaps
-// Be sure to add an overlay Layer containing the earthquake GeoJSON
-L.control.layers(baseMaps).addTo(myMap);
-};
+  // L.marker([location]).addTo(myMap);
+  d3.csv("/static/data/potholes_clean_mnthfinal.csv").then((data) => {
+    if (markerGroup) myMap.removeLayer(markerGroup);
+   setTimeout(()=>{
+    markerGroup = L.layerGroup().addTo(myMap);
+    // let filteredData = data.filter(elt => elt.CREATION_YEAR == dataSelection);
+    let filterCoord = filteredData.map(data => data.LOCATION.split(","));
+    console.log(typeof filterCoord[0]);
+    for (var i = 0; i < 100; i++) {
+        var location = filterCoord[i];
+        console.log(location);
+        L.marker([location[0],location[1]]).addTo(markerGroup);
+      };
+   },0)
+  
+  })};
 
 function buildBarChart(dataSelection,filteredData) {
+
+  // clear/overwrite previous data
+  let barchart = d3.select("#top_x_div")
+  barchart.html(""); 
   
   console.log(dataSelection);
   console.log(filteredData);
@@ -226,6 +189,11 @@ function buildBarChart(dataSelection,filteredData) {
 
 
 function buildLineChart(dataSelection, filteredData) {
+
+  // clear/overwrite previous data
+  let linechart = d3.select("#chart_div")
+  linechart.html(""); 
+
   console.log(dataSelection);
   console.log(filteredData);
 
